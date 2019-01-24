cca15b5b26c3e2e03f55a0d975392d0938cacb84
Ethan Lyons <ethan.lyons84@gmail.com>
2019-01-24 16:36:42 -0700

    Added output to.gitignore

--- src/edu/cnm/deepdive/GuessANumber.java
+++ src/edu/cnm/deepdive/GuessANumber.java
@@ -16,33 +16,41 @@ public class GuessANumber {
     int attempts;
     boolean surrender;
 
-    again = true;
-    while (again) {
-      numberToGuess = random.nextInt(100) + 1;
-      found = false;
-      surrender = false;
-      while (!found && !surrender) {
-        int myGuess;
-        System.out.println("Please enter a guess");
-        myGuess = input.nextInt();
-        if (myGuess == 0) {
-          surrender = true;
-        } else if (myGuess == numberToGuess) {
-          System.out.println(
-              "Congragulations your guess was correct. The number was " + numberToGuess + "");
-          found = true;
-        } else if (myGuess > numberToGuess) {
-          System.out.println("Your guess is too high");
-        } else {
-          System.out.println("Your guess is too low");
-        }
+
+    while (playOneRound());
+  }
+
+  private static boolean playOneRound() {
+    int numberToGuess;
+    boolean found;
+    boolean surrender;
+    boolean again =true;
+
+    String response;
+    numberToGuess = random.nextInt(100) + 1;
+    found = false;
+    surrender = false;
+    while (!found && !surrender) {
+      int myGuess;
+      System.out.println("Please enter a guess");
+      myGuess = input.nextInt();
+      if (myGuess == 0) {
+        surrender = true;
+      } else if (myGuess == numberToGuess) {
+        System.out.println("Congratulations your guess was correct. The number was " + numberToGuess + "");
+        found = true;
+      } else if (myGuess > numberToGuess) {
+        System.out.println("Your guess is too high");
+      } else {
+        System.out.println("Your guess is too low");
       }
-      System.out.println("Would you like to play again?");
-      do {
-        response = input.nextLine(). trim() .toLowerCase();
-      } while (response.isEmpty());
-      again = response.charAt(0) == 'y';
-      System.out.println("thank you for playing");
     }
+    System.out.println("Would you like to play again?");
+    do {
+      response = input.nextLine(). trim() .toLowerCase();
+    } while (response.isEmpty());
+    System.out.println("thank you for playing");
+
+    return response.charAt(0) == 'y';
   }
 }
